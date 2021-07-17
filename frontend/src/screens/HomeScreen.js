985e42dd35aef6a4221fc0b3788b88c27ac3df0a
Adarsh Jaiswal <40723757+AdarshJais@users.noreply.github.com>
2021-07-18 00:58:15 +0530

    redux state into HomeScreen useDispatch and useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+// import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
+  // const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    const fetchProducts = async () => {
-      // Our backend is running on port:5000 and teh fronted at port:3000 do when we do /api/products it will look in port 3000 that will definetly throw error and we also can't directly breach out to another port so In such situation we use Proxy from package.json
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+    // const fetchProducts = async () => {
+    //   // Our backend is running on port:5000 and teh fronted at port:3000 do when we do /api/products it will look in port 3000 that will definetly throw error and we also can't directly breach out to another port so In such situation we use Proxy from package.json
+    //   const { data } = await axios.get('/api/products');
+    //   setProducts(data);
+    // };
+    // fetchProducts();
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
